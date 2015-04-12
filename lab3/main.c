e06acee50aa37f645c06523c40839f7497fdcd68
Никита Толстяков <geleox@MacBook-Pro-Nikita.local>
2015-04-12 19:24:46 +0300

    Initial Commit

---
+++ lab3/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  lab3
+//
+//  Created by Никита Толстяков on 12.04.15.
+//  Copyright (c) 2015 Никита Толстяков. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
